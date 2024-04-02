b3f771ce078f3bdd1b7c363e2d7f096bc5e17884
JaydenMeekoly <129750476+JaydenMeekoly@users.noreply.github.com>
2024-04-02 21:04:41 +0200

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Models;
+
+namespace OnlineStore.Controllers
+{
+    public class UserController : Controller
+    {
+        public Table_1 tbl1 = new Table_1();
+
+        [HttpPost]
+        public ActionResult About(Table_1 Users)
+        {
+            var result = tbl1.InsertUser(Users);
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View("~/Views/Users/About.cshtml",tbl1);
+        }
+        
+    }
+}
---
+++ Models/Table_1.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore.Models
+{
+    public class Table_1 : Controller
+    {
+        public static string con_string = "Server=tcp:cloud-server.database.windows.net,1433;Initial Catalog=Cldv-DB1;Persist Security Info=False;User ID=JaydenServerAdminLogin;Password=JayMeeko01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static SqlConnection con = new SqlConnection(con_string);
+
+        public String Name { get; set; }
+
+        public String Surname { get; set; }
+
+        public String Email { get; set; }
+
+        public int InsertUser(Table_1 n)
+        {
+
+
+                string sql = "insert into Table_1 (user_Name , user_Surname, user_Email) VALUES (@Name, @Surname, @Email)";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Name", n.Name);
+                cmd.Parameters.AddWithValue("@Surname", n.Surname);
+                cmd.Parameters.AddWithValue("@Email", n.Email);
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                con.Close();
+                return rowsAffected;
+        }
+            
+
+        
+    }
+
+    }
