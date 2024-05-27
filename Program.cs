b34001ab50fb45feab309dbfa83166edfde8c01c
JaydenMeekoly <129750476+JaydenMeekoly@users.noreply.github.com>
2024-05-27 18:29:47 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddSession(Options=>
+{
+    Options.IdleTimeout = TimeSpan.FromMinutes(120);
+});
 
 var app = builder.Build();
 
@@ -12,6 +17,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
